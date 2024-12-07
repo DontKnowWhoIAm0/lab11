63122f1ecbaf77b2169fae40b8b5d831ecdb2fbe
Kamila <shakurova.06kamila@gmail.com>
2024-12-07 10:21:50 +0300

    Шакурова

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,12 +13,12 @@ public class Main {
 
         System.out.println("Пользователи:");
         for(int i = 0; i < users.length; i++) {
-            System.out.println(users[i]);
+            System.out.println(users[i].getNickName());
         }
 
         System.out.println("Сообщения: ");
         for(int i = 0; i < messages.length; i++) {
-            System.out.println(messages[i]);
+            System.out.println("Отправитель"  + messages[i].getSender() + " Получатель " + messages[i].getRecipient() + " Текст " + messages[i].getText() + " Дата и Время " + messages[i].getDataAndTime());
         }
     }
 }
