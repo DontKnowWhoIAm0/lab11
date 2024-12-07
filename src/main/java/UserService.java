bfa95d3cbe11e3e70a176b69d1acdcabf66aec4c
DontKnowWhoIAm0 <sofyakolodeznikova@gmail.com>
2024-12-07 09:38:59 +0300

    Колодезникова - класс UserService

--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -1,8 +1,22 @@
 public class UserService {
 
+/*  ник нейм
+    - имя пользователя
+    - телефон
+    - пароль
+
+ */
     public User[] create4Users() {
         User[] users = new User[4];
-
-        User first = new User("")
+        User first = new User("dont_know_who_I_am", "Соня", "+79120124565", "qwerty1234");
+        users[0] = first;
+        User second = new User("atubien", "Ляйсан", "+79123456789", "Lays");
+        users[1] = second;
+        User third = new User("arinkmm", "Арина", "+79379874536", "fghksdkf");
+        users[2] = third;
+        User fourth = new User("Kkkkkkkkkk066", "Камила", "+79024569878", "password");
+        users[3] = fourth;
+        return users;
     }
+
 }
