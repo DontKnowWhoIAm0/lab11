a44d4370d34f1fd2cb31a9e4ba00af3dd2be175e
Arinkmm <mairabeeva42@gmail.com>
2024-12-07 09:32:15 +0300

    Класс Message - Майрабеева

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
-
 public class Message {
+    private String sender;
+    private String recipient;
+    private String text;
+    private String dataAndTime;
+
+    public Message() {}
+
+    public Message(String sender, String recipient, String text, String dataAndTime) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.text = text;
+        this.dataAndTime = dataAndTime;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public void setDataAndTime(String dataAndTime) {
+        this.dataAndTime = dataAndTime;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getDataAndTime() {
+        return dataAndTime;
+    }
 }
+
